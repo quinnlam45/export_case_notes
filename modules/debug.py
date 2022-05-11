4d6946666784f060f8abd03d710a85f3653f3d43
quinn.lam <quinnlam45@gmail.com>
2022-05-11 12:59:23 +0100

    changed build report function

--- modules/debug.py
+++ modules/debug.py
@@ -1,3 +1,4 @@
+from asyncore import write
 from cgi import test
 from fileinput import filename
 import io
@@ -43,33 +44,6 @@ def return_pivoted_dfs(df, df_rows, df_cols):
     return dfs_list
 
 
-def build_report(df_list):
-
-    current_row = 0
-    buffer = io.BytesIO()
-
-    with NamedTemporaryFile(suffix='.xlsx') as tmp:
-
-        # func in excel module?
-        with pd.ExcelWriter(tmp) as writer:
-            for df in df_list:
-                df.to_excel(writer, startrow=current_row+1, sheet_name='sheet', na_rep = 0)
-                no_rows_in_df = return_no_of_df_rows(df)
-                current_row += no_rows_in_df
-            writer.save()
-
-        tmp.seek(0)
-        stream = tmp.read()
-        buffer.write(stream)
-
-    return buffer
-
-def bytes_to_file(stream, filename):
-    with open(filename, 'wb') as ex_file:
-        stream.seek(0)
-        ex_file.write(stream.read())
-
-
 cols = 'Service risk level'
 rows_to_pivot = ['Service area', 'Referrer', 'Gender', 'Ethnicity']
 
@@ -83,25 +57,54 @@ for risk_level in risk_category:
     filenames.append(file_name)
 
 ex_file = 'test_output.xlsx'
-# wb = Workbook()
-# wb.save(ex_file)
+
+# def apply_index_format():
+#     return "background-color: yellow;"
+
+def write_to_excel_multiple_sheets(file_obj):
+    with pd.ExcelWriter(file_obj) as writer:
+
+        for f in filenames: # run each query
+            current_row = 0
+            data = read_excel_file(f) # get data method
+            df = pd.DataFrame(data[1:], columns=data[0])
+
+            pivoted_df_list = return_pivoted_dfs(df, rows_to_pivot, cols)
+
+            sh_name = f[:-5]
+
+            for df in pivoted_df_list:
+                # df_style = df.style
+                # df_style.applymap_index(apply_index_format)
+
+                df.to_excel(writer, startrow=current_row+1, sheet_name=sh_name, na_rep=0)
+                no_rows_in_df = return_no_of_df_rows(df)
+                current_row += no_rows_in_df
+
+        writer.save()
 
 
-with pd.ExcelWriter(ex_file) as writer: #  mode='a', if_sheet_exists='replace'
-    wb = writer.book
-    # print(ws.title)
+def build_report(excel_writer):
 
-    for f in filenames:
-        current_row = 0
-        data = read_excel_file(f)
-        df = pd.DataFrame(data[1:], columns=data[0])
-        pivoted_df_list = return_pivoted_dfs(df, rows_to_pivot, cols)
+    current_row = 0
+    buffer = io.BytesIO()
 
-        sh_name = f[:-5]
+    with NamedTemporaryFile(suffix='.xlsx') as tmp:
 
-        for df in pivoted_df_list:
-            df.to_excel(writer, startrow=current_row+1, sheet_name=sh_name, na_rep=0)
-            no_rows_in_df = return_no_of_df_rows(df)
-            current_row += no_rows_in_df
+        #implement different excelwriter/to excel methods here
+        excel_writer(tmp)
 
-    writer.save()
+        tmp.seek(0)
+        stream = tmp.read()
+        buffer.write(stream)
+
+    return buffer
+
+def bytes_to_file(stream, filename):
+    with open(filename, 'wb') as ex_file:
+        stream.seek(0)
+        ex_file.write(stream.read())
+
+
+stream = build_report(write_to_excel_multiple_sheets)
+bytes_to_file(stream, ex_file)
